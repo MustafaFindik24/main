5fe373ba61b36104b318b1d01d827acde4f4ad47
MustafaFındık <91599453+MustafaFindik24@users.noreply.github.com>
2022-04-25 16:41:08 +0300

    First commit

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -4,6 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestMain {
     @Test
     public void testInputIsEven(){
-        assertTrue(Main.sayıKontrol(202)); // Assertion
+        assertTrue(Main.sayıKontrol(200)); // Assertion
     }
 }
